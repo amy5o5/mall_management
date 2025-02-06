3930832d41d9e1f02fb5e8c28f924b18b730f28a
amin283 <yeganemohammadamin@gmail.com>
2025-02-06 21:59:01 +0330

    just testing

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+app.get("/login", (req,res) => {
+  res.render('login-register');
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://192.168.1.183:3000");
 });

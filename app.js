c838313ca5dc4c73f9fb4edbdb2d45800146efd5
amin283 <yeganemohammadamin@gmail.com>
2025-02-11 21:55:05 +0330

    admin and user access bug fixed

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const authMiddleware = require("./middlewares/auth.middleware");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-// const db_connect = require('./database/db_connect');
-require('dotenv').config();
+
+//const db_connect = require('./database/db_connect');
+require("dotenv").config();
 app.set("view engine", "ejs");
 
-app.use(cookieParser());  // قرار دادن cookie-parser قبل از middleware‌های دیگر
+app.use(cookieParser()); // قرار دادن cookie-parser قبل از middleware‌های دیگر
 app.use(express.static("public"));
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", authMiddleware, (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-const adminRoute = require('./routes/admin.route');
-app.use('/admin', adminRoute);
+const adminRoute = require("./routes/admin.route");
+app.use("/admin", adminRoute);
 
-const authRoute = require('./routes/auth.route');
-app.use('/auth', authRoute);
+const authRoute = require("./routes/auth.route");
+app.use("/auth", authRoute);
 
-const IP = process.env.IP || "127.0.0.1";
+const IP = "127.0.0.1"; //process.env.IP ||
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, IP, () => {
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const db_connect = require("../database/db_connect");
-const authMiddleware = require("./../middlewares/auth.middleware");
+const checkAccess = require('./../middlewares/check-actor.middleware')
 
-router.get("/panel",authMiddleware, (req, res) => {
+
+router.get("/panel",checkAccess(["admin"]), (req, res) => {
   res.render("admin", { error: null });
 });
 
